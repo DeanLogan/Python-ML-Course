c7d3cc1e43b841ffb6703d581299083bebb8b767
Dean Logan <61757204+DeanLogan123@users.noreply.github.com>
2023-05-21 11:03:23 +0100

    Finished Health Cost Calculator project

--- Core Learning Algorithms/Linear Regression/titanic survivial predictions.py
+++ Core Learning Algorithms/Linear Regression/titanic survivial predictions.py
@@ -48,7 +48,7 @@ NUMERIC_COLUMNS = ['age', 'fare']
 
 feature_columns = []
 for feature_name in CATEGOIRCAL_COLUMNS:
-    vocabulary = dftrain[feature_name].unique() #gets a list of all unique values from given feature column
+    vocabulary = dftrain[feature_name].unique() # gets a list of all unique values from given feature column
     feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
 
 for feature_name in NUMERIC_COLUMNS:
@@ -58,13 +58,13 @@ for feature_name in NUMERIC_COLUMNS:
 
 # Creating the input function
 def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
-  def input_function(): # inner function, this will be returned
-    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df)) # create tf.data.Dataset object with data and its label
-    if shuffle:
-      ds = ds.shuffle(1000) # randomize order of data
-    ds = ds.batch(batch_size).repeat(num_epochs) # split dataset into batches of 32 and repeat process for number of epochs
-    return ds # return a batch of the dataset
-  return input_function # return a function object for use
+    def input_function(): # inner function, this will be returned
+        ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df)) # create tf.data.Dataset object with data and its label
+        if shuffle:
+            ds = ds.shuffle(1000) # randomize order of data
+        ds = ds.batch(batch_size).repeat(num_epochs) # split dataset into batches of 32 and repeat process for number of epochs
+        return ds # return a batch of the dataset
+    return input_function # return a function object for use
 
 train_input_fn = make_input_fn(dftrain, y_train) # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
 eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
--- Projects/Health Costs Calculator/healthCostCalc.py
+++ Projects/Health Costs Calculator/healthCostCalc.py
@@ -9,3 +9,70 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 FILE_DIR = os.path.join(ROOT_DIR, 'insurance.csv')
 dataset = pd.read_csv(FILE_DIR)
 print(dataset.head())
+
+# change any strings to ints
+dataset["sex"] = pd.factorize(dataset["sex"])[0]
+dataset["region"] = pd.factorize(dataset["region"])[0]
+dataset["smoker"] = pd.factorize(dataset["smoker"])[0]
+
+dftest = dataset.sample(frac=0.2) # selects 20% of the df randomly
+#dftest = dataset.tail(round(len(dataset)*0.2)) # uncomment this line and comment the line above to use the last 20% of the dataset as the training data
+dftrain = dataset[~dataset.isin(dftest)].dropna() # removes all the records within dftrain from dataset and stores this as dftest
+
+# remove expenses fromand store
+train_labels = dftrain.pop('expenses')
+test_labels = dftest.pop('expenses')
+
+print(dftrain.head())
+
+# normalize the data and 
+normalizer = tf.keras.layers.Normalization()
+normalizer.adapt(np.array(dftrain))
+
+model = tf.keras.Sequential([
+    normalizer,
+    tf.keras.layers.Dense(26),
+    tf.keras.layers.Dropout(0.1),
+    tf.keras.layers.Dense(1),
+])
+
+model.compile(
+    optimizer=tf.optimizers.Adam(learning_rate=0.05),
+    loss='mae',
+    metrics=['mae', 'mse']
+)
+model.build()
+model.summary()
+
+history = model.fit(
+    dftrain,
+    train_labels,
+    epochs=225,
+    validation_split=0.4,
+    verbose=0, # Suppress logging
+)
+
+print(history)
+
+# RUN THIS CELL TO TEST YOUR MODEL. DO NOT MODIFY CONTENTS.
+# Test model by checking how well the model generalizes using the test set.
+loss, mae, mse = model.evaluate(dftest, test_labels, verbose=2)
+
+print("Testing set Mean Abs Error: {:5.2f} expenses".format(mae))
+
+if mae < 3500:
+    print("You passed the challenge. Great job!")
+else:
+    print("The Mean Abs Error must be less than 3500. Keep trying.")
+
+# Plot predictions.
+test_predictions = model.predict(dftest).flatten()
+
+a = plt.axes(aspect='equal')
+plt.scatter(test_labels, test_predictions)
+plt.xlabel('True values (expenses)')
+plt.ylabel('Predictions (expenses)')
+lims = [0, 50000]
+plt.xlim(lims)
+plt.ylim(lims)
+_ = plt.plot(lims,lims)
